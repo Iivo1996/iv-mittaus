1b0a39342e822ae0e76d22f816af76956aec2e57
Iivo1996 <65504433+Iivo1996@users.noreply.github.com>
2026-09-20 09:42:19 +0300

    Päivitä välimuisti uudelle kuvakkeelle

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="iv-mittaus-v10";
-const APP_SHELL=["./","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+const CACHE_NAME="iv-mittaus-v11";
+const APP_SHELL=["./","./manifest.webmanifest","./app-icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{
