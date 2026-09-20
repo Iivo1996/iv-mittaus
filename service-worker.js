1c3693062eeef063b5832c430f82c1cd119226cb
Iivo1996 <65504433+Iivo1996@users.noreply.github.com>
2026-09-20 09:52:00 +0300

    Lisää valittu puhallinkuvake PWA-sovellukseen

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="iv-mittaus-v11";
-const APP_SHELL=["./","./manifest.webmanifest","./app-icon.svg"];
+const CACHE_NAME="iv-mittaus-v12";
+const APP_SHELL=["./","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{
