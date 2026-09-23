505bf7fd1814695d390f371545bc6f9b071f2a1f
Iivo1996 <65504433+Iivo1996@users.noreply.github.com>
2026-09-23 18:36:11 +0300

    Refresh cache for expanded saved entry

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="iv-mittaus-v29";
+const CACHE_NAME="iv-mittaus-v30";
 const APP_SHELL=["./","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png","./airduo-logo.jpg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
